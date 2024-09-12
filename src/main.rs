8b60358826c285a7057b5874ec976b7b4c59e7c0
unco <1054093062@qq.com>
2024-09-13 03:24:34 +0800

    fix

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+#![recursion_limit = "256"]
+#![feature(specialization)]
+#![feature(trait_alias)]
+#![feature(generic_const_exprs)]
+#![feature(const_type_id)]
+#![feature(associated_type_defaults)]
+#![feature(unboxed_closures)]
+#![feature(unsize)]
+#![feature(const_trait_impl)]
+#[warn(incomplete_features)]
+use bevy_mask_system::MaskSys;
+use mask_system_lib::{*};
+use bevy::{
+    app::{App, Startup, Update}, ecs::{component::Components, query::{QueryData, QueryFilter}}, math::bool, prelude::{default, Commands, Component, Entity, IntoSystem, Query, QueryBuilder, With, Without}, reflect::DynamicTuple, ui::shader_flags::BORDER, utils::all_tuples, DefaultPlugins
+};
+
+fn main() {
+    println!("Hello, world!");
+}
+
+
+#[derive(MaskSys)]
+struct Weapon;
